8017c90e89c2e263b0a320cb47f9c8d4ea9afe55
CHEEMA <cchukwu588@gmail.com>
2025-07-10 01:31:00 +0100

    Update firebase.js to use Railway FIREBASE_SERVICE_ACCOUNT env

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // firebase.js (place this in the root directory)
 
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
